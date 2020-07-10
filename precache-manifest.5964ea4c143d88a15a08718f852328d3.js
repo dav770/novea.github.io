917babea11da383ecf05303654df1bcf99ae46fd
david cassuto <cassuto.david770@gmail.com>
2020-07-10 13:36:41 +0300

    Updates

---
+++ precache-manifest.5964ea4c143d88a15a08718f852328d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "364dd0ade4c0d5d783b3e3769ee3e6fb",
+    "url": "/dav770/novea.github.io.git/index.html"
+  },
+  {
+    "revision": "76c30fdafe7515882ac0",
+    "url": "/dav770/novea.github.io.git/static/css/2.fd83e43b.chunk.css"
+  },
+  {
+    "revision": "e6a3636eb0e48dc283bd",
+    "url": "/dav770/novea.github.io.git/static/css/main.42591f24.chunk.css"
+  },
+  {
+    "revision": "76c30fdafe7515882ac0",
+    "url": "/dav770/novea.github.io.git/static/js/2.0070d880.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/dav770/novea.github.io.git/static/js/2.0070d880.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6a3636eb0e48dc283bd",
+    "url": "/dav770/novea.github.io.git/static/js/main.76a8b3fd.chunk.js"
+  },
+  {
+    "revision": "e7441a8beac663cb449b",
+    "url": "/dav770/novea.github.io.git/static/js/runtime-main.4d91b0db.js"
+  }
+]);
